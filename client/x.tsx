f6e4595179793979da6296b2996f1ba339590694
Ohanronnie <titiloyepaul68@gmail.com>
2023-06-22 19:46:42 +0100

    changes

--- client/x.tsx
+++ client/x.tsx
@@ -17,6 +17,7 @@ import {
   Outlet,
 } from "react-router-dom";
 import axios from "./utils/axios.ts";
+
 function ProtectedRoute() {
   const [loading, setLoading] = useState<boolean>(true);
   const [valid, setValid] = useState<boolean>(false);
@@ -45,18 +46,38 @@ export default function Router() {
           <Route path="register/signup" element={<Signup />} />
           <Route path="register/login" element={<Login />} />
           <Route path="register/token" element={<Token />} />
-          <Route path="home" element={<ProtectedRoute />}>
-            <Route path="" element={<Home />} />
-          </Route>
-          <Route path="chats/:userId" element={<ProtectedRoute />}>
-            <Route path="" element={<Chats />} />
-          </Route>
-          <Route path="adduser" element={<ProtectedRoute />}>
-            <Route path="" element={<AddChats />} />
-          </Route>
-          <Route path="upload" element={<ProtectedRoute />}>
-            <Route path="" element={<Upload />} />
-          </Route>
+          <Route
+            path="home"
+            element={
+              <ProtectedRoute>
+                <Home />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="chats/:userId"
+            element={
+              <ProtectedRoute>
+                <Chats />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="adduser"
+            element={
+              <ProtectedRoute>
+                <AddChats />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="upload"
+            element={
+              <ProtectedRoute>
+                <Upload />{" "}
+              </ProtectedRoute>
+            }
+          />
           <Route path="imageupload/:to/:from" element={<ProtectedRoute />}>
             <Route path="" element={<ImageUpload />} />
           </Route>
